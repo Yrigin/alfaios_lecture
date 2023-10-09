79094737b78153c1b04084debd172b0f801f375f
PetrYrigin <68337082+YriginPetr@users.noreply.github.com>
2023-10-09 18:45:28 +0300

    update

--- alfaios_lecture/ContentView.swift
+++ alfaios_lecture/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "people")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world!  HI")
         }
         .padding()
     }
