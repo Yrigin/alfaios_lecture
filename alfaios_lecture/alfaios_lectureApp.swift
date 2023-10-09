6f2c7604445686507439eefdb6576ff7368753d7
KarenMkrtchyan666 <68337082+YriginPetr@users.noreply.github.com>
2023-10-09 15:20:33 +0300

    Initial Commit

---
+++ alfaios_lecture/alfaios_lectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  alfaios_lectureApp.swift
+//  alfaios_lecture
+//
+//  Created by Пётр Ярыгин on 09.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct alfaios_lectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
